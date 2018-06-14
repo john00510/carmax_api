645fa1181291896a1af22f041400a10bed4a9d2d
yuri <yury0051@gmail.com>
2018-06-14 10:23:42 +0200

    views.py changed

--- carmax_api/views.py
+++ carmax_api/views.py
@@ -27,7 +27,7 @@ def carmax_api_make():
 @app.route("/api/model")
 def carmax_api_model():
     make = request.args.get('make')
-    query = "SELECT DISTINCT model FROM CARS WHERE make = '{}'".format(make)
+    query = "SELECT model, COUNT(model) FROM CARS WHERE make = '{}' GROUP BY model".format(make)
     cur.execute(query)
     data = [x[0] for x in cur.fetchall()]
     return json.dumps(data)
